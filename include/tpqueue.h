08ac0feea3a2740c43d88327053a79a95229d001
Zhilin21-IST-4 <94217280+Zhilin21-IST-4@users.noreply.github.com>
2022-05-08 18:18:58 +0300

    Update tpqueue.h

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -6,38 +6,41 @@
 template<typename T, int size>
 class TPQueue {
 
-private:
+ private:
   T arr[100];
   int first;
   int last;
+  
 public:
-  TPQueue() : first(0), last(0) {}
-  int getSize() {
-    return last - first;
+    TPQueue() : first(0), last(0) {}
+    int getSize() {
+      return last - first;
     }
     void push(T temporary) {
       if (getSize() >= size) {
         throw std::string("full"); // не работает
-          } else {
-            int i = last++;
-            while ((--i >= first) && (arr[i % size].prior < temporary.prior)) {
-              arr[(i + 1) % size] = arr[i % size];
-              }
-              arr[(i + 1) % size] = temporary;
-          }
-      }
-      T front() {
-      return arr[(first) % size];
-  }
-  T back() {
-    return arr[(last - 1) % size];
+      } else {
+          int i = last++;
+          while ((--i >= first) && (arr[i % size].prior < temporary.prior)) {
+            arr[(i + 1) % size] = arr[i % size];
+        }
+        arr[(i + 1) % size] = temporary;
+       }
+    }
+    T front() {
+        return arr[(first) % size];
+    }
+    T back() {
+        return arr[(last - 1) % size];
     }
     T pop() {
-    return arr[(first++) % size];
+        return arr[(first++) % size];
     }
- };
+};
+
 struct SYM {
   char ch;
   int prior;
 };
+
 #endif  // INCLUDE_TPQUEUE_H_
